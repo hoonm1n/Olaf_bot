772a90bfcd0ee504cb857704e50806d8188d2eec
qwertyBBeers <gudgh1630@gmail.com>
2022-11-01 16:44:59 +0900

    MinJi_nav

---
+++ Olaf_bot/aws-robomaker-hospital-world-ros1/build/aws_robomaker_hospital_world/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/minji/catkin_ws/src/aws-robomaker-hospital-world-ros1/build/aws_robomaker_hospital_world/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/usr/local/cuda-11.3/lib64'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/minji/catkin_ws/src/aws-robomaker-hospital-world-ros1/build/aws_robomaker_hospital_world/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/minji/catkin_ws/src/aws-robomaker-hospital-world-ros1:$ROS_PACKAGE_PATH"
