22ea1b30403ac6e41d356f82fec492724eb3ce06
qwertyBBeers <gudgh1630@gmail.com>
2022-11-01 21:04:05 +0900

    Omo_to_Olaf

--- Olaf_bot/aws-robomaker-hospital-world-ros1/build/aws_robomaker_hospital_world/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/minji/catkin_ws/src/aws-robomaker-hospital-world-ros1
-# Build directory: /home/minji/catkin_ws/src/aws-robomaker-hospital-world-ros1/build/aws_robomaker_hospital_world
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- Olaf_bot/aws-robomaker-hospital-world-ros1/build/aws_robomaker_hospital_world/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/minji/catkin_ws/src/aws-robomaker-hospital-world-ros1/build/aws_robomaker_hospital_world/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
