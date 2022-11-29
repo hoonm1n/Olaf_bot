157dc9433dba7b40f5a3e5afb389214aa53b92af
qwertyBBeers <gudgh1630@gmail.com>
2022-11-29 16:18:09 +0900

    11.29 change odom

--- Olaf_bot/Olaf_bringup/src/velCal.cpp
+++ Olaf_bot/Olaf_bringup/src/velCal.cpp
@@ -18,7 +18,6 @@ ros::Time current_time, last_time;
 
 void CmdVelCallback(const geometry_msgs::Twist::ConstPtr& msg){
     vel_x = msg->linear.x;
-    vel_x = -vel_x;
     vel_th = msg->angular.z;
 }
 
