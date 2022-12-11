cc645e357378e11f8d6dfea82af57752beab58d5
psi0000 <qkrwk0921@naver.com>
2022-12-11 21:45:14 +0900

    12.11 app and nav

--- Olaf_bot/Olaf_bringup/src/olaf_push.py
+++ Olaf_bot/Olaf_bringup/src/olaf_push.py
@@ -5,40 +5,63 @@ from std_msgs.msg import String
 from std_msgs.msg import Bool
 from Olaf_bringup.srv import Appdata
 
+global roompose  
+global recievemethod
+
 roompose = ""
 recievemethod = ""
+# class Input:
+#     def __init__(self):
+#         roompose 
+#         recievemethod
 
-def talker():
-    rate = rospy.Rate(10) # 10hz
+# def talker():
+#     rate = rospy.Rate(10) # 10hz
 
-    arr_room = Float32MultiArray()
-    arr_room.data = [101, 102, 103]
+#     arr_room = Float32MultiArray()
+#     arr_room.data = [101, 102, 103]
 
-    rospy.loginfo("When you press on 'k', your robot will go to the goal!!!")
+#     rospy.loginfo("When you press on 'k', your robot will go to the goal!!!")
 
-    while not rospy.is_shutdown():
-        if recievemethod == "2":
-            rospy.loginfo("gogo!")
-            tu_pub.publish(arr_room)
+#     while not rospy.is_shutdown():
+#         print(recievemethod)
+#         if recievemethod == "2":
+#             rospy.loginfo("gogo!")
+#             tu_pub.publish(arr_room)
 
-        rate.sleep()
+#         rate.sleep()
 
 def handle_add_two_ints(Appdata):
     roompose = Appdata.input1
     recievemethod = Appdata.input2
+    print(recievemethod)
+    arr_room = Float32MultiArray()
+    arr_room.data = [101, 102, 103]
+
+    rospy.loginfo("When you press on 'k', your robot will go to the goal!!!")
+    print(recievemethod)
+    if recievemethod == "2":
+        rospy.loginfo("gogo!")
+        tu_pub.publish(arr_room)
+
+    rate.sleep()
     return "ok"
     
 def add_two_ints_server():
-    rospy.init_node('add_two_ints_server')
+    #rospy.init_node('add_two_ints_server')
     s = rospy.Service('/Appdata', Appdata, handle_add_two_ints)
     print("Ready to add two ints.")
     
     
 if __name__ == '__main__':
+    rospy.init_node('add_two_ints_server')
+
+    rate = rospy.Rate(10) # 10hz
+
     try:
         add_two_ints_server()
         tu_pub = rospy.Publisher('room_info', Float32MultiArray, queue_size=10)
-        talker()
+        #talker()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
